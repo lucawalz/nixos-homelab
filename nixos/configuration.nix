ec030ef8a8dab96d03be27984d37907ead32dd2a
Walz Luca <lw138@hdm-stuttgart.de>
2025-10-26 16:47:49 +0100

    Set KUBECONFIG for all users

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -117,6 +117,11 @@ in
     ];
   };
 
+  # Set KUBECONFIG environment variable for all users
+  environment.variables = {
+    KUBECONFIG = "/etc/rancher/k3s/k3s.yaml";
+  };
+
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [

b541a254c2ebd60292fb0c633f61ce60c9af178a
Luca Walz <148595155+lucawalz@users.noreply.github.com>
2026-04-15 22:31:22 +0200

    chore: add worker-1 stable host key, apply all node config fixes

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -4,7 +4,7 @@ let
   # Host SSH public keys (ed25519)
   # These are the SSH host keys from /etc/ssh/ssh_host_ed25519_key.pub on each machine
   master = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAU9eaUVbsNWFhRKfzokIBEWY7mfAmb+ISf3kjVKHErx root@master";
-  worker-1 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJ9ZMnrZHyY0aE72y6boCYgrUYdX9mMH3r1vWxlSZPbb root@worker-1";
+  worker-1 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBsCOF/GD5lCxesfVwG6DHGRQQCdAX5F4vld9yyk+3jR root@worker-1";
   worker-2 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGCdX2yJ94TwBRr/lRbFax4xguXQGcHM2AhaFnV3UVCw root@worker-2";
   
   luca = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHoKFFTFmJR1CSAq55TwXHbUPTxSK847qZL0W6r/ZUV9 luca@macbook";

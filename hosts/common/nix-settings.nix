c2d6a2ee10cfe894299d81f0d9926ac2d9df18ef
Luca Walz <148595155+lucawalz@users.noreply.github.com>
2026-04-15 22:19:18 +0200

    fix: trusted-users, master-only helm/flux, static host, remove node-exporter, longhorn 3 replicas, github SSH deploy key   config

--- hosts/common/nix-settings.nix
+++ hosts/common/nix-settings.nix
@@ -4,4 +4,10 @@
     experimental-features = [ "nix-command" "flakes" ];
     trusted-users = [ "root" "@wheel" ];
   };
+
+  programs.ssh.extraConfig = ''
+    Host github.com
+      IdentityFile /etc/ssh/ssh_host_ed25519_key
+      IdentitiesOnly yes
+  '';
 }
